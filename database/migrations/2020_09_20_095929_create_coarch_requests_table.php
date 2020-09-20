a9aba8bf4b08fbb5990f7011c6542828060134e8
kakashi <nk29time@gmail.com>
2020-09-20 20:26:14 +0900

    first commit

---
+++ database/migrations/2020_09_20_095929_create_coarch_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoarchRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coarch_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('sports_id')->references('id')->on('sports');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coarch_requests');
+    }
+}
