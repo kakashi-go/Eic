ef3823689f6bc5a652e73144d80d3616d5dd5c35
kakashi <nk29time@gmail.com>
2020-09-24 20:56:07 +0900

    first commit

--- resources/views/coach_request/show.blade.php
+++ resources/views/coach_request/show.blade.php
@@ -1,19 +1,17 @@
 <h1>詳細画面</h1>
-<p><a href="{{ route('sport.index')}}">一覧画面</a></p>
+<p><a href="{{ route('coach_request.index')}}">一覧画面</a></p>
  
 <table border="1">
     <tr>
         <th>id</th>
         <th>name</th> 
-        <th>coach_request</th>
         <th>created_at</th>
         <th>updated_at</th>
     </tr>
     <tr>
-        <td>{{ $sport->id }}</td>
-        <td>{{ $sport->name }}</td> 
-        <td>{{ $sport->coach_request}}</td> 
-        <td>{{ $sport->created_at }}</td>
-        <td>{{ $sport->updated_at }}</td>
+        <td>{{ $coach_request->id }}</td> 
+        <td>{{ $coach_request->name }}</td> 
+        <td>{{ $coach_request->created_at }}</td>
+        <td>{{ $coach_request->updated_at }}</td>
     </tr>
 </table>
--- resources/views/sport/index.blade.php
+++ resources/views/sport/index.blade.php
@@ -1,5 +1,5 @@
 <h1>一覧画面</h1>
-<p><a href="{{ route('sports.create') }}">新規追加</a></p>
+<p><a href="{{ route('sport.create') }}">新規追加</a></p>
  
 @if ($message = Session::get('success'))
 <p>{{ $message }}</p>
@@ -15,8 +15,8 @@
     @foreach ($sports as $sport)
     <tr>
         <td>{{ $sport->name }}</td>
-        <th><a href="{{ route('sport.show',$sports->id)}}">詳細</a></th>
-        <th><a href="{{ route('sport.edit',$sports->id)}}">編集</a></th>
+        <th><a href="{{ route('sport.show',$sport->id)}}">詳細</a></th>
+        <th><a href="{{ route('sport.edit',$sport->id)}}">編集</a></th>
         <th>
             <form action="{{ route('sport.destroy', $sport->id)}}" method="POST">
                 @csrf
